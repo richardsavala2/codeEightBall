a4aa94d30a0048fee2b22e5b57cc1e2583277a09
richard savala <richardsavala@my.cwi.edu>
2020-10-29 07:25:43 +0000

    added online resources and words of encouragement to the values.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function eightBall(){
     const answer = Math.floor(Math.random() * 8 + 1);
-    var fortunes = ["odinProject.com", "freeCodeCamp.com", "Udemy.com", "leetCode.com", "algoExpert.com", "techInterviewPro.com", "commit daily.", "take a break."];
+    const fortunes = ["odinProject.com", "freeCodeCamp.com", "Udemy.com", "leetCode.com", "algoExpert.com", "techInterviewPro.com", "commit daily.", "take a break."];
 
-    console.log(fortunes[answer]);
+    fortune = fortunes[answer];
+    console.log(fortune);
 }
 
 eightBall()
